4388d68c50bde0cd20e8d27a39a41e3f46f7ecfd
11shimazuM <95361235+11shimazuM@users.noreply.github.com>
2022-02-11 02:41:17 +0900

    Delete Machi-Kaji0210 directory

--- Machi-Kaji0210/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:dbname=gs_db;charset=utf8;host=localhost','root','root');
-  } catch (PDOException $e) {
-    exit('DbConnectError:'.$e->getMessage());
-  }
-  ?>
